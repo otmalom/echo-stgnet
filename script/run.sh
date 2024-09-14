b9c3dd361eecdbd090600c35d7b224bea3be6af5
otmalom <57488290+otmalom@users.noreply.github.com>
2024-09-14 22:11:20 +0800

    Update run.sh

--- script/run.sh
+++ script/run.sh
@@ -1,12 +1,13 @@
 dataset=camus_4ch
 backbone=ResNet34
-predictor=STCPredictor
+predictor=STGCNPredictor
+loss_fn=a
 npts=46
 topk=5
 device=0
-nohup python main.py --train --device ${device} --num_kpts ${npts} --top_k ${topk}  --sample_rate 1.0 --engine temporal \
+nohup python main.py --train --resume --loss_fn ${loss_fn} --device ${device} --num_kpts ${npts} --top_k ${topk} --sample_rate 1.0 --engine tem_seq \
 --backbone_name ${backbone} \
 --predictor_name ${predictor} \
 --cfg_path lib/config/config_sgd.yaml \
---data_path Data/${dataset} \
+--data_path /home/lihh/Projects/data/${dataset} \
 --proj_path experiments/${dataset}/${backbone}_${predictor}_${npts}pts_${topk}top > nohup/${dataset}/${backbone}_${predictor}_${npts}pts_${topk}top.out 2>&1 &
