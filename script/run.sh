5bfd8393c89f12cc7debb768ed1777dbeba8e820
otmalom <57488290+otmalom@users.noreply.github.com>
2024-09-14 22:05:53 +0800

    Update run.sh

--- script/run.sh
+++ script/run.sh
@@ -5,3 +5,8 @@ npts=46
 topk=5
 device=0
 nohup python main.py --train --device ${device} --num_kpts ${npts} --top_k ${topk}  --sample_rate 1.0 --engine temporal \
+--backbone_name ${backbone} \
+--predictor_name ${predictor} \
+--cfg_path lib/config/config_sgd.yaml \
+--data_path Data/${dataset} \
+--proj_path experiments/${dataset}/${backbone}_${predictor}_${npts}pts_${topk}top > nohup/${dataset}/${backbone}_${predictor}_${npts}pts_${topk}top.out 2>&1 &
